e07094a27f6d3710c75489c40fbb09b272fcbce1
x-io Technologies <social@x-io.co.uk>
2025-03-05 11:05:05 +0000

    Update ximu3csv

--- Examples/logged_lower_body.py
+++ Examples/logged_lower_body.py
@@ -3,24 +3,22 @@ import time
 
 import imumocap.solvers
 import models
-import numpy as np
-import scipy
 import ximu3csv
 from imumocap import Matrix
 
 dont_block = "dont_block" in sys.argv  # don't block when script run by CI
 
 # Import IMU data
-devices = ximu3csv.read("Logged Lower Body", [ximu3csv.DataMessageType.QUATERNION])
+devices = ximu3csv.read("Logged Lower Body", ximu3csv.DataMessageType.QUATERNION)
 
 # Resample IMU data and arrange as dictionary of device names
-first_timestamp = np.max([d.quaternion.timestamp[0] for d in devices]) / 1e6  # convert microseconds to seconds
-last_timestamp = np.min([d.quaternion.timestamp[-1] for d in devices]) / 1e6
-
 FPS = 30
-timestamps = np.arange(first_timestamp, last_timestamp, 1 / FPS)
 
-imus = {d.device_name: scipy.interpolate.interp1d(d.quaternion.timestamp / 1e6, d.quaternion.quaternion.wxyz, axis=0)(timestamps) for d in devices}
+devices = ximu3csv.resample(devices, FPS)
+
+number_of_samples = len(devices[0].quaternion.timestamp)
+
+imus = {d.device_name: d.quaternion.quaternion.wxyz for d in devices}
 
 # Load example model
 model = models.LowerBody()
@@ -31,7 +29,7 @@ imumocap.solvers.calibrate(model.root, {n: Matrix(quaternion=q[0, :]) for n, q i
 # Create animation frames
 frames = []
 
-for index, _ in enumerate(timestamps):
+for index in range(number_of_samples):
     for link in model.root.flatten():
         if link.name in imus:
             link.set_joint_from_imu_global(Matrix(quaternion=imus[link.name][index, :]))
