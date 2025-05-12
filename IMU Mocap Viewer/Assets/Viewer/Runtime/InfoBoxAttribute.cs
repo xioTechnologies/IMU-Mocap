0dbee8acf434276ac4947fe76e3acd6d8a5924c1
Phill Tew <me@philltew.com>
2025-05-12 21:28:01 +0100

    Add inspector info box

---
+++ IMU Mocap Viewer/Assets/Viewer/Runtime/InfoBoxAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Viewer.Runtime
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class InfoBoxAttribute : PropertyAttribute
+    {
+        public readonly string Message;
+
+        public readonly MessageType Type;
+
+        public InfoBoxAttribute(string message, MessageType type = MessageType.Info)
+        {
+            Message = message;
+
+            Type = type;
+        }
+    }
+
+    public enum MessageType
+    {
+        None,
+        Info,
+        Warning,
+        Error,
+    }
+}
