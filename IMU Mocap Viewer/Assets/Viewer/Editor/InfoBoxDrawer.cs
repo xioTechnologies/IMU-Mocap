d0d0f0c23691982def94c4497334f74c8d6e6952
Phill Tew <me@philltew.com>
2025-05-13 09:54:06 +0100

    Add inspector info box

---
+++ IMU Mocap Viewer/Assets/Viewer/Editor/InfoBoxDrawer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Viewer.Runtime;
+using MessageType = Viewer.Runtime.MessageType;
+
+namespace Viewer.Editor
+{
+    [CustomPropertyDrawer(typeof(InfoBoxAttribute))]
+    public class InfoBoxDrawer : DecoratorDrawer
+    {
+        InfoBoxAttribute Info => attribute as InfoBoxAttribute;
+        private float width = 200;
+
+        static UnityEditor.MessageType Convert(MessageType type) =>
+            type switch
+            {
+                MessageType.None => UnityEditor.MessageType.None,
+                MessageType.Info => UnityEditor.MessageType.Info,
+                MessageType.Warning => UnityEditor.MessageType.Warning,
+                MessageType.Error => UnityEditor.MessageType.Error,
+                _ => UnityEditor.MessageType.None
+            };
+
+        public override void OnGUI(Rect position)
+        {
+            EditorGUI.HelpBox(position, Info.Message, Convert(Info.Type));
+
+            width = EditorGUIUtility.currentViewWidth;
+        }
+
+        public override float GetHeight()
+        {
+            var style = EditorStyles.helpBox;
+
+            var content = new GUIContent(Info.Message);
+
+            float height = style.CalcHeight(content, width);
+
+            return height + EditorGUIUtility.singleLineHeight;
+        }
+    }
+}
