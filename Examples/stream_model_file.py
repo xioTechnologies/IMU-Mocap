4ca11f0f2e3914e6a824ae5ad3b564de7e33b8b0
x-io Technologies <social@x-io.co.uk>
2025-06-19 11:19:48 +0100

    Add stream model file example

---
+++ Examples/stream_model_file.py
@@ -0,0 +1,33 @@
+import time
+
+import hardware
+import imumocap
+import imumocap.file
+import imumocap.solvers
+
+# Load example model
+root, joints = imumocap.file.load_model("stream_model_file.json")
+
+calibraiton_pose = imumocap.get_pose(root)
+
+# Connect to and configure IMUs
+imus = hardware.setup([l.name for l in root.flatten() if l.name])
+
+# Stream to IMU Mocap Viewer
+connection = imumocap.viewer.Connection()
+
+while True:
+    time.sleep(1 / 30)  # 30 fps
+
+    if any([i.button_pressed for i in imus.values()]):
+        print("Please hold the calibraiton pose")
+
+        time.sleep(2)
+
+        imumocap.solvers.calibrate(root, {n: i.matrix for n, i in imus.items()}, calibraiton_pose)
+
+        print("Done")
+
+    imumocap.set_pose_from_imus(root, {n: i.matrix for n, i in imus.items()})
+
+    connection.send(imumocap.viewer.link_to_primitives(root))
