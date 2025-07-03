dac9c90d31fbe56a4bf5f06b1825ee4e0ceca826
Phill Tew <me@philltew.com>
2025-07-03 10:16:32 +0100

    Add persistent settings

---
+++ IMU Mocap Viewer/Assets/Viewer/Runtime/Global/GlobalSettingToggle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Viewer.Runtime.Global
+{
+    [RequireComponent(typeof(Toggle))]
+    public class GlobalSettingToggle : MonoBehaviour
+    {
+        [SerializeField] private GlobalSetting setting;
+
+        private Toggle toggle;
+
+        private void Awake()
+        {
+            toggle = GetComponent<Toggle>();
+
+            toggle.isOn = setting.Value;
+
+            toggle.onValueChanged.AddListener(ToggleChanged);
+            setting.OnValueChanged += SettingChanged;
+        }
+
+        private void OnDestroy()
+        {
+            toggle.onValueChanged.RemoveListener(ToggleChanged);
+            setting.OnValueChanged -= SettingChanged;
+        }
+
+        private void SettingChanged(bool value)
+        {
+            if (toggle.isOn != value) toggle.isOn = value;
+        }
+
+        private void ToggleChanged(bool value)
+        {
+            if (setting.Value != value) setting.Value = value;
+        }
+    }
+}
