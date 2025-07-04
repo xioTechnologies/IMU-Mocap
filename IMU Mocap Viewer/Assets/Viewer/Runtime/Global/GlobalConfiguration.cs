d5b89589996b3c720b8c8084e618e563c0b87a8e
Phill Tew <me@philltew.com>
2025-07-04 07:19:52 +0100

    Fix event registration bug

--- IMU Mocap Viewer/Assets/Viewer/Runtime/Global/GlobalConfiguration.cs
+++ IMU Mocap Viewer/Assets/Viewer/Runtime/Global/GlobalConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Viewer.Runtime.Global
@@ -7,14 +8,28 @@ namespace Viewer.Runtime.Global
     {
         [InfoBox("Settings persist in the player preferences"), SerializeField]
         private List<GlobalSetting> settings = new();
-
+        
+        private readonly Dictionary<GlobalSetting, Action<bool>> events = new();
+        
         void Awake()
         {
             foreach (var setting in settings)
             {
                 if (PlayerPrefs.HasKey(setting.ID)) setting.Value = PlayerPrefs.GetInt(setting.ID) != 0;
 
-                setting.OnValueChanged += value => PlayerPrefs.SetInt(setting.ID, value ? 1 : 0);
+                Action<bool> settingEvent = value => PlayerPrefs.SetInt(setting.ID, value ? 1 : 0);
+
+                events[setting] = settingEvent; 
+
+                setting.OnValueChanged += settingEvent;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var setting in settings)
+            {
+                setting.OnValueChanged -= events[setting];
             }
         }
     }
